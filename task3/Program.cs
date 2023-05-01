a03f4da0a83387f834f1e90ae52bfe93f9c164b8
Maxim Shapovalov <Maxim.A.Shapovalov@gmail.com>
2023-05-01 19:58:16 +0300

    add task3

---
+++ task3/Program.cs
@@ -0,0 +1,40 @@
+﻿//Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+int Promt(string message)
+{
+    Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+bool IsWeekDay(int IsWeekDay)
+{
+    if (IsWeekDay > 5)
+    {
+        return true;
+    }
+    return false;
+}
+
+bool ValidateWeekday(int number)
+{
+    if (number > 0 && number <=7)
+    {
+        return true;
+    }
+    Console.WriteLine("Это не день недели");
+    return false;
+}
+
+int IsWeekDay = Promt("Введите день недели ");
+if (ValidateWeekday(weekDay))
+{
+    if (IsWeekend(weekDay))
+    {
+        Console.WriteLine("Выходной");
+    }
+    else
+    {
+        Console.WriteLine("Рабочий день");
+    }
+}
